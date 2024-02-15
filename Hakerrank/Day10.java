1a4466758a222a2637eef511943714095ba408c3
ellinm <46395859+ELLINM@users.noreply.github.com>
2024-02-15 22:15:02 +0900

    Update Day10.java

--- Hakerrank/Day10.java
+++ Hakerrank/Day10.java
@@ -18,3 +18,29 @@ public class Solution {
         
     }
 }
+
+/*
+Error Test Case 
+if H and B is negative 
+if H or B is Zero
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int B = sc.nextInt();
+        int H = sc.nextInt();
+        
+        int square = B*H;
+        if(B > 0 && H > 0){
+            System.out.print(square);
+        }else if(B <= 0 || H <= 0){
+            System.out.print("java.lang.Exception: Breadth and height must be positive");
+        }        
+    }
+}
