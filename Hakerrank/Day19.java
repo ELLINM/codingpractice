099a7d14653d517a5e7861b91e0a4dcf2852fd5e
ellinm <46395859+ELLINM@users.noreply.github.com>
2024-02-26 21:02:50 +0900

    Create Day19.java

---
+++ Hakerrank/Day19.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.trim();
+        String[] arrS = s.split("[!,?._ '@]+");
+        
+        int num = arrS.length;
+        if(arrS == null){
+            System.out.println(0);
+        }else{
+            System.out.println(num);
+        }
+        for(int i = 0; i < arrS.length; i++){
+            System.out.println(arrS[i]);
+        }
+        scan.close();
+    }
+}
+
